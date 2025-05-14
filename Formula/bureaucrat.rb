697a2ae28b057884642335d17cf0e595284ca291
Arttu Perälä <arttu@perala.me>
2025-05-14 20:59:14 +0000

    bureaucrat: add 0.1.0 bottle.

--- Formula/bureaucrat.rb
+++ Formula/bureaucrat.rb
@@ -6,6 +6,13 @@ class Bureaucrat < Formula
   license "MIT"
   head "https://github.com/arttuperala/bureaucrat.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/arttuperala/homebrew-bureaucrat/releases/download/bureaucrat-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8df17844c1e0846da42e5c0b0109cb0835f6ab8a759b395858a195ed52082712"
+    sha256 cellar: :any_skip_relocation, ventura:       "9b1b5063b428b503105b656f0743ff337e6fbab8ff8efae155437a95c5ff9e51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "817732ba73d345eeff2900cd724641d940e41217b4d4b400d1480ccfc2bd2997"
+  end
+
   depends_on "rust" => :build
 
   uses_from_macos "zlib"
